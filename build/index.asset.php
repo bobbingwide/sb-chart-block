cc367e60acb5a4592ad72fe84a3d47c65f3f6021
Herb Miller <herb@bobbingwide.com>
2022-12-01 16:35:05 +0000

    Update build for label and x-axis font size #21

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '049e798e41041e4a8996');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2d65f57ac3676accfbb6');
