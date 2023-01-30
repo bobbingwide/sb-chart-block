79c737e53ad4ff75e5b005732d2b066d1450f0fd
Herb Miller <herb@bobbingwide.com>
2023-01-30 12:34:20 +0000

    Improve sb_chart_block_array_get() to allow for lower cased attributes for shortcodes #21

--- sb-chart-block.php
+++ sb-chart-block.php
@@ -109,6 +109,9 @@ function sb_chart_block_dynamic_block( $attributes ) {
 /**
  * Returns the array index or default.
  *
+ * To allow for invocation by shortcode, where attribute names are lower cased, not case sensitive,
+ * this now tests for the mixed case $index first, then the lower case version.
+ *
  * @param $array
  * @param $index
  * @param null $default
@@ -117,8 +120,11 @@ function sb_chart_block_dynamic_block( $attributes ) {
  */
 function sb_chart_block_array_get( $array, $index, $default=null ) {
 	if ( isset( $array ) ) {
+		$lc_index = strtolower( $index );
 		if ( isset( $array[ $index ] ) ) {
 			$value = $array[ $index ];
+		} elseif ( isset( $array[ $lc_index]) ) {
+			$value = $array[ $lc_index ];
 		} else {
 			$value = $default;
 		}
@@ -140,7 +146,7 @@ function sb_chart_block_array_get( $array, $index, $default=null ) {
  */
 function sb_chart_block_get_csv( $data, $empty_to_null = false, $min_nb_fields = 0, $new_item_value = '' ) {
 	$array = [];
-	
+
 	if ( is_string( $data ) && '' !== $data ) {
 		$array = str_getcsv( $data );
 		if ($empty_to_null) {
@@ -151,12 +157,12 @@ function sb_chart_block_get_csv( $data, $empty_to_null = false, $min_nb_fields =
 			}
 		}
 	}
-	
+
 	$nb_new_fields = $min_nb_fields - count( $array );
-	for ( $i = 0; $i < $nb_new_fields; $i++ ) { 
+	for ( $i = 0; $i < $nb_new_fields; $i++ ) {
 		$array[] = $new_item_value;
 	}
-	
+
 	return $array;
 }
 
@@ -193,13 +199,13 @@ function sb_chart_block_array_replace( $array1, $array2 ) {
  */
 function sb_chart_block_merge_objects( $object1, $object2, $default=null ) {
 	$value = null;
-	
+
 	if ( is_object( $object1 ) && is_object( $object2 ) ) {
 		$value = (object)array_merge((array)$object1, (array)$object2);
 	} else {
 		$value = $default;
 	}
-	
+
 	return $value;
 }
 
