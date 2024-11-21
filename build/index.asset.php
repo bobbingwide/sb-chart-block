c925e705d2ad925acd7d89774446880382db78bc
Herb Miller <herb@bobbingwide.com>
2024-11-21 16:36:50 +0000

    Update package version and rebuild #37

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6832b06d0d9fafd520c3');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a3537bdea5a3d014f3dd');
