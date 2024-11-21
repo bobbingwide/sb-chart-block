394abc6912b6b0d8a1bfda52c81ec5a0f356e013
Herb Miller <herb@bobbingwide.com>
2024-11-21 15:47:04 +0000

    Extract deprecated to separate file #37

--- src/chart-block/index.js
+++ src/chart-block/index.js
@@ -26,8 +26,12 @@ import './style.scss';
  */
 import edit from './edit';
 import save from './save';
+import { useBlockProps } from '@wordpress/block-editor';
 
 import { getDefaultTheme } from './theme-colors';
+
+import deprecated from './deprecated';
+
 
 /**
  * Every block starts by registering a new block type definition.
@@ -41,7 +45,7 @@ registerBlockType( 'oik-sb/chart', {
 		attributes: {
 			type: "pie",
 			content: __( "Label,Value\nOne,1\nTwo,2\nThree,3", 'sb-chart-block' ),
-			myChartId: "myChartExample"
+			//myChartId: "myChartExample"
 		}
 	},
 
@@ -55,29 +59,6 @@ registerBlockType( 'oik-sb/chart', {
 	 */
 	save,
 
-	deprecated: [
-		{
-			attributes: {
-				type: { type: 'string', default: 'line', },
-				content: { type: 'string', default: '', },
-				theme: { type: 'string', default: getDefaultTheme() },
-				myChartId: { type: 'string', default: 'myChart-' },
-				stacked: { type: 'boolean', default: false },
-				fill: { type: 'boolean', default: false },
-				height: { type: 'integer', default: null },
-				beginYAxisAt0: { type: 'boolean', default: false },
-				opacity: { type: 'number', default: 0.8 }
-			},
-			supports: {html: false, align: false },
-
-			save( props ) {
-
-				return <div className={"chartjs"} style={{height: props.attributes.height}}>
-						<canvas id={props.attributes.myChartId}></canvas>
-					</div>
-
-			}
-		}
-	]
+	deprecated
 
 } );
