f3423f20729102ca88a96eac295aa20989fd8894
Herb Miller <herb@bobbingwide.com>
2024-11-22 11:37:46 +0000

    Set version to 1.3.0 #37

--- sb-chart-block.php
+++ sb-chart-block.php
@@ -3,7 +3,7 @@
  * Plugin Name:     SB Chart block
  * Plugin URI: 		https://www.oik-plugins.com/oik-plugins/sb-chart-block
  * Description:     Displays a Chart for CSV content
- * Version:         1.2.6
+ * Version:         1.3.0
  * Author:          bobbingwide
  * Author URI: 		https://www.bobbingwide.com/about-bobbing-wide
  * License:         GPL-2.0-or-later
